386aca27415c6e75c915dd44a9d9f15fb9246212
teric622 <tlasecaeric@gmail.com>
2023-04-12 03:40:03 -0400

    pushing to branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "./App.css";
+import { Col, Row, Container } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript - Welcome to our
-                Project! - Shay Patel, Eric Tlaseca - Morales, Joshua Taing,
-                Arjun Manikyath, Stephen Wiafe, Thomas Pelosi
-            </header>
+            <Container>
+                <Row>
+                    <Col>
+                        <header className="App-header">
+                            UD CISC275 with React Hooks and TypeScript - Welcome
+                            Project! - Shay Patel, Eric Tlaseca - Morales,
+                            Joshua Taing, Arjun Manikyath, Stephen Wiafe, Thomas
+                            Pelosi
+                        </header>
+                    </Col>
+                    <Col>
+                        <header className="App-">
+                            UD CISC275 with React Hooks and TypeScript - Welcome
+                            Project! - Shay Patel, Eric Tlaseca - Morales,
+                            Joshua Taing, Arjun Manikyath, Stephen Wiafe, Thomas
+                            Pelosi
+                        </header>
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
