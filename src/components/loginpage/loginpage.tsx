62a2494563ee6d0aebe8fe720edbd685082e4b3f
stephenwiafe <stephwiafe18@gmail.com>
2023-04-13 12:42:40 -0400

    Add content to page and postion with flex

--- src/components/loginpage/loginpage.tsx
+++ src/components/loginpage/loginpage.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
+import ronaldo from "./ronaldo.webp";
 
 function Login(): JSX.Element {
     return (
-        <div className="App">
-            <h2>login page</h2>
+        <div className="login">
+            <img src={ronaldo} />
+            <div>
+                <div className="header">Welcome to Ekin</div>
+                <div>Sign in</div>
+                <div>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control
+                            type="email"
+                            placeholder="Email or Username"
+                        />
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button>LOGIN NOW</Button>
+                    <div>Forgot your password?</div>
+                    <Button>Sign in with Google</Button>
+                </div>
+                <div className="last-buttons">
+                    <Button>Make account</Button>
+                    <Button>Log in as Admin</Button>
+                </div>
+            </div>
         </div>
     );
 }
