0e2c4a2964676762a803e4a1b2d4d95b0a0d13c9
stephenwiafe <stephwiafe18@gmail.com>
2023-04-22 12:10:05 -0400

    Add profile

--- src/components/makeaccount/makeaccount.tsx
+++ src/components/makeaccount/makeaccount.tsx
@@ -1,73 +1,13 @@
 import React from "react";
 import "./makeaccount.css";
+import { Button, Form } from "react-bootstrap";
 
 function Makeaccount(): JSX.Element {
     return (
         <div className="App">
-            <h2>Account Creation </h2>
-            {/* <p>Please click the button below to create account.</p>
-
-            <button>Sign Up</button>
-
-            <div id="id01" className="modal">
-                <span className="close" title="Close Modal">
-                    times;
-                </span>
-                <form className="modal-content" action="/action_page.php">
-                    <div className="container">
-                        <h1>Sign Up</h1>
-                        <p>Please fill in this form to create an account.</p>
-                        <br />
-                        <label htmlFor="email">
-                            <b>Email</b>
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Enter Email"
-                            name="email"
-                            required
-                        ></input>
-                        <label htmlFor="psw">
-                            <b>Password</b>
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="Enter Password"
-                            name="psw"
-                            required
-                        ></input>
-                        <label htmlFor="psw-repeat">
-                            <b>Repeat Password</b>
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="Repeat Password"
-                            name="psw-repeat"
-                            required
-                        ></input>
-                        <label>
-                            <input
-                                type="checkbox"
-                                checked={false}
-                                name="remember"
-                            >
-                                {" "}
-                                Remember me{" "}
-                            </input>
-                        </label>
-                        <p>By creating an account you agree to our </p>
-                        <a href="#">Terms and Privacy</a>.
-                        <div className="clearfix">
-                            <button type="button" className="cancelbtn">
-                                Cancel
-                            </button>
-                            <button type="submit" className="signup">
-                                Sign Up
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div> */}
+            <div className="profile">
+                <img></img>
+            </div>
         </div>
     );
 }
