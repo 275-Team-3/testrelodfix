f1838b19de38e994b3c6f8d56de6c754f276e456
teric622 <tlasecaeric@gmail.com>
2023-04-25 16:18:01 -0400

    firebase integrated and google authentication succesfully implemented

--- src/components/loginpage/loginpage.tsx
+++ src/components/loginpage/loginpage.tsx
@@ -2,8 +2,28 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 import Ronaldo from "./r7.jpg";
 import "./loginpage.css";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/messaging";
+import "firebase/compat/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function Login(): JSX.Element {
+    firebase.initializeApp({
+        apiKey: "AIzaSyDuuO3Qvj_guTliM6iPgV3_5dF0OWOLi-0",
+        authDomain: "proj-15d75.firebaseapp.com",
+        projectId: "proj-15d75",
+        storageBucket: "proj-15d75.appspot.com",
+        messagingSenderId: "1096734466692",
+        appId: "1:1096734466692:web:c7cbf5f3fd67c3bf7c58d1",
+        measurementId: "G-WWLTRVX79F"
+    });
+    const authenticate = firebase.auth();
+    const authenticationService = new firebase.auth.GoogleAuthProvider();
+
+    const signInBox = () => {
+        authenticate.signInWithPopup(authenticationService);
+    };
     return (
         <div className="login">
             <div className="login">
@@ -27,7 +47,7 @@ function Login(): JSX.Element {
                     </div>
                     <h6>Forgot your password?</h6>
                     <div>
-                        <Button>Sign in with Google</Button>
+                        <Button onClick={signInBox}>Sign in with Google</Button>
                     </div>
                 </div>
                 <div className="last-buttons">
@@ -35,6 +55,7 @@ function Login(): JSX.Element {
                     <Button>Log in as Admin</Button>
                 </div>
             </div>
+            {/* <section>{user ? console.log("is in") : console.log("no")}</section> */}
         </div>
     );
 }
