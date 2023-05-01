767db424f8853b4d1c3aa23c6fbdb451af155521
patelsap <patelsap@udel.edu>
2023-05-01 10:55:52 -0400

    cart half complete

--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -2,9 +2,38 @@ import React from "react";
 import Navbar from "../navbarcomponent/navbar";
 function Checkout(): JSX.Element {
     return (
-        <div className="App">
+        <div>
             <Navbar />
-            <h2>checkout</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Product</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Socks</th>
+                        <td>Nike</td>
+                        <td>$12.99</td>
+                        <td>#000001</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">T-Shirt</th>
+                        <td>Adidas</td>
+                        <td>$24.99</td>
+                        <td>#000002</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Shorts</th>
+                        <td>Puma</td>
+                        <td>$19.99</td>
+                        <td>#000003</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
